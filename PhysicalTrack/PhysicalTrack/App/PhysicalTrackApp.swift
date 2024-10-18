608775ac78b4581d0bf5ddafa8a33bd181e45cc5
Melt <seokwoo2000@naver.com>
2024-10-18 11:57:18 +0900

    [Set] 프로젝트 초기 설정

---
+++ PhysicalTrack/PhysicalTrack/App/PhysicalTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhysicalTrackApp.swift
+//  PhysicalTrack
+//
+//  Created by 장석우 on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct PhysicalTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WorkoutView()
+        }
+    }
+}
