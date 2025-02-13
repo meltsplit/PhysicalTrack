1116cb42676707eab59b8e0ea0969088bdd882db
Melt <seokwoo2000@naver.com>
2025-02-13 22:33:56 +0900

    [Del] #55 - AnyPTLocation 코드 삭제

--- PhysicalTrack/PhysicalTrack/Domain/Location.swift
+++ PhysicalTrack/PhysicalTrack/Domain/Location.swift
@@ -30,7 +30,6 @@ struct Location {
 extension Location: Equatable {
     static func == (lhs: Location, rhs: Location) -> Bool {
         ObjectIdentifier(lhs.rawValue) == ObjectIdentifier(rhs.rawValue)
-        
     }
 }
 
@@ -60,41 +59,3 @@ extension Location {
         )
     }
 }
-
-//struct StubLocation: LocationType {
-//    var stubDistance: Double
-//    var speed: Double
-//    var timestamp: Date
-//    func distance(from location: Self) -> Double {
-//        return stubDistance
-//    }
-//}
-
-//struct AnyLocation {
-//    public var base: any LocationType
-//    
-//    public init<L>(_ base: L) where L : LocationType {
-//        self.base = base
-//    }
-//}
-//
-//extension AnyLocation: Equatable {
-//    static func == (lhs: AnyLocation, rhs: AnyLocation) -> Bool {
-//        lhs.base.speed == rhs.base.speed &&
-//        lhs.base.timestamp == rhs.base.timestamp
-//    }
-//}
-//
-//extension AnyLocation: LocationType {
-//    var speed: Double {
-//        base.speed
-//    }
-//    
-//    var timestamp: Date {
-//        base.timestamp
-//    }
-//    
-//    func distance(from location: AnyLocation) -> Double {
-//        base.distance(from: location.base)
-//    }
-//}
