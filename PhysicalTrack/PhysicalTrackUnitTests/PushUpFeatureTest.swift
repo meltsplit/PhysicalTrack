495abb32821d44c9984952dd8051dc549fff8204
Melt <seokwoo2000@naver.com>
2025-02-19 22:49:21 +0900

    [Test] #61 - pushup 테스트 코드 주석 해제

--- PhysicalTrack/PhysicalTrackUnitTests/PushUpFeatureTest.swift
+++ PhysicalTrack/PhysicalTrackUnitTests/PushUpFeatureTest.swift
@@ -6,113 +6,100 @@
 //
 
 import Testing
-import ComposableArchitecture
-@testable import PhysicalTrack
 
-//@MainActor
-//struct PushUpFeatureTest {
-//    
-//    @Test
-//    func 푸시업기록_엔티티의_기간은_120초이다() async {
-//        #expect(PushUpRecord(for: .elite).duration == .seconds(120))
-//    }
-//    
-//    
-//    @Test
-//    func test_when_onAppear_then_ready_3초후_시작() async {
-//        let clock = ImmediateClock()
-//        let store = TestStore(
-//            initialState: PushUpFeature.State(
-//                PushUpRecord.init(
-//                    duration: .seconds(120),
-//                    targetCount: 72,
-//                    count: 0,
-//                    tempo: []
-//                )
-//            )) {
-//                PushUpFeature()
-//            } withDependencies: {
-//                $0.continuousClock = clock
-//            }
-//        
-//        store.exhaustivity = .off
-//        
-//        await store.send(.onAppear)
-//        await store.receive(\.ready)
-//        await store.receive(\.readyTimerTick) { $0.readyLeftSeconds = 2 }
-//        await store.receive(\.readyTimerTick) { $0.readyLeftSeconds = 1 }
-//        await store.receive(\.readyTimerTick) { $0.readyLeftSeconds = 0 }
-//        await store.receive(\.readyTimerTick)
-//        await store.receive(\.start)
-//        await store.finish()
-//        
-//    }
-//    
-//    @Test
-//    func test_종료버튼을누르면_타이머가_중단된다() async {
-//        let clock = TestClock()
-//        let store = TestStore(initialState: PushUpFeature.State(
-//            PushUpRecord.init(
-//                duration: .seconds(120),
-//                targetCount: 72,
-//                count: 0,
-//                tempo: []
-//            )
-//        )) {
-//            PushUpFeature()
-//        } withDependencies: {
-//            $0.proximityClient = .testValue
-//            $0.continuousClock = clock
-//        }
-//        
-//        store.exhaustivity = .off
-//        
-//        await store.send(.start)
-//        await store.send(.quitButtonTapped)
-//        await store.receive(\.pause)
-//        
-//        await clock.advance(by: .seconds(7))
-//        
-//        await store.finish()
-//    }
-//    
-//    //
-//    //
-//    //    @Test
-//    //    func test_종료버튼을_누르면_ProximityClient를_cancel한다() async {
-//    //        let clock = TestClock()
-//    //        let proximityStream = AsyncStream.makeStream(of: Bool.self)
-//    //        let stubProximityStream = ProximityClient(
-//    //            start: {@Sendable in proximityStream.stream },
-//    //            stop: { @Sendable in proximityStream.continuation.finish() }
-//    //        )
-//    //        let store = TestStore(initialState: PushUpFeature.State(
-//    //            .stub(duration: .seconds(120), targetCount: 10, count: 0, tempo: [])
-//    //        )) {
-//    //            PushUpFeature()
-//    //        } withDependencies: {
-//    //            $0.proximityClient = stubProximityStream
-//    //            $0.continuousClock = clock
-//    //        }
-//    //
-//    //        store.exhaustivity = .off
-//    //
-//    //        await store.send(.start) {
-//    //            $0.record.count = 0
-//    //        }
-//    //
-//    //        await store.send(.quitButtonTapped)
-//    //        await store.receive(\.pause)
-//    //
-//    //        let before = store.state.record.count
-//    //        // 무효화 되어야 할 운동 ---
-//    //        proximityStream.continuation.yield(true) //영
-//    //        proximityStream.continuation.yield(false) //차
-//    //        // ---
-//    //        let after = store.state.record.count
-//    //
-//    //        #expect(before == after)
-//    //        await store.finish()
-//    //    }
-//    //}
-//}
+@testable import PhysicalTrack
+import ComposableArchitecture
+
+@MainActor
+struct PushUpFeatureTest {
+    
+    @Test
+    func 푸시업기록_엔티티의_기간은_120초이다() async {
+        #expect(PushUpRecord(for: .elite).duration == .seconds(120))
+    }
+    
+    @Test
+    func 푸시업뷰_진입시_준비타이머_3초후_운동이_시작된다() async {
+        let clock = ImmediateClock()
+        let store = TestStore(
+            initialState: PushUpFeature.State(
+                PushUpRecord.stub()
+            )) {
+                PushUpFeature()
+            } withDependencies: {
+                $0.continuousClock = clock
+            }
+        
+        store.exhaustivity = .off
+        
+        await store.send(.onAppear)
+        await store.receive(\.ready)
+        await store.receive(\.readyTimerTick) { $0.readyLeftSeconds = 2 }
+        await store.receive(\.readyTimerTick) { $0.readyLeftSeconds = 1 }
+        await store.receive(\.readyTimerTick) { $0.readyLeftSeconds = 0 }
+        await store.receive(\.readyTimerTick)
+        await store.receive(\.start)
+        await store.finish()
+        
+    }
+    
+    @Test
+    func 정지버튼을_누르면_실제시간이_흘러도_운동시간은_증가하지_않는다() async {
+        let clock = TestClock()
+        let store = TestStore(initialState: PushUpFeature.State(
+            PushUpRecord.stub()
+        )) {
+            PushUpFeature()
+        } withDependencies: {
+            $0.proximityClient = .testValue
+            $0.continuousClock = clock
+        }
+        
+        store.exhaustivity = .off
+        
+        await store.send(.start)
+        await store.send(.quitButtonTapped)
+        await store.receive(\.pause)
+        
+        let before = store.state.workoutLeftSeconds
+        await clock.advance(by: .seconds(7))
+        let after = store.state.workoutLeftSeconds
+        
+        #expect(before == after)
+    }
+    
+    @Test
+    func 정지버튼을_누르면_근접센서에_값이_들어와도_운동횟수는_증가하지_않는다() async {
+        let clock = TestClock()
+        let (stream, continuation) = AsyncStream.makeStream(of: Bool.self)
+        
+        let store = TestStore(
+            initialState:
+                PushUpFeature.State(.stub())
+        ) {
+            PushUpFeature()
+        } withDependencies: {
+            $0.proximityClient.start = { @Sendable in stream }
+            $0.proximityClient.stop = { @Sendable in continuation.finish() }
+            $0.continuousClock = clock
+        }
+        
+        store.exhaustivity = .off
+        
+        await store.send(.start) {
+            $0.record.count = 0
+        }
+        
+        await store.send(.quitButtonTapped)
+        await store.receive(\.pause)
+        
+        let before = store.state.record.count
+        // 무효화 되어야 할 운동 ---
+        continuation.yield(true) //영
+        continuation.yield(false) //차
+        // ---
+        let after = store.state.record.count
+        
+        #expect(before == after)
+    }
+}
